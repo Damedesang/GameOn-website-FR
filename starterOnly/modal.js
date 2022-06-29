7ae90c01d774e5ab0e17e0cfeb90d6481e457cdd
Marine P <72661857+Damedesang@users.noreply.github.com>
2022-06-29 22:36:59 +0200

    Derniere avancé sur le projet
    Modification apres reprise du projet.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,68 @@ function editNav() {
   }
 }
 
-// DOM Elements
+//#region DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//#endregion
 
-// launch modal event
+// #region launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// #endregion
 
-// launch modal form
+// #region launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// #endregion
 
+// #region Vérification et validation du formulaire
+function validationFormulaire(){
+  if (validationRadioBox()){
+    //alert('Merci');
+    document.getElementById("formulaire").submit();
+    //validé l'envoi et affiché message d'envoi reussi.
+  }
+  else {
+    alert('Vous devez choisir une option.');
+    //Ne pas validé l'envoi
+  }
+  
+}
+// Fonction qui check si au moins un des bouton est cochée
+function validationRadioBox(){
+  if (document.getElementById("location1").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location2").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location3").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location4").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location5").checked == true){
+    return true;
+  }
+  else if (document.getElementById("location6").checked == true){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+// #endregion
 
+// #region changement de modal apres envoi du formulaire
+function changeModal(){
+  alert('changeModal');
+  launchModal();
+  document.getElementById("modal-body").style.display = "none";
+  document.getElementById("modal-validation").style.display = "block";
+  alert('changeModalend');
+}
+
+//#endregion
